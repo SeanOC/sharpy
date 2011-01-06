f9007d806338ccf74174225b963a74616491e65e
Sean O'Connor <sean@saaspire.com>
2011-01-06 16:40:47 -0500

    Added a basic setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Sharpy',
+    version='0.1',
+    description='Python client for the Cheddar Getter API (http://cheddargetter.com).',
+    author="Sean O'Connor",
+    author_email="sean@saaspire.com",
+    url="https://github.com/Saaspire/sharpy",
+    packages=['sharpy'],
+)
