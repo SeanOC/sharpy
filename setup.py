0fc8cfd5b084ec20aa33e9bbf9d99ed9b8223efe
Sean O'Connor <sean@seanoc.com>
2011-05-16 16:35:47 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='Sharpy',
-    version='0.6.10',
+    version='0.7.0',
     description='Python client for the Cheddar Getter API (http://cheddargetter.com).',
     author="Sean O'Connor",
     author_email="sean@saaspire.com",
